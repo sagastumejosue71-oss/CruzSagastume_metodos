9c932f30f982671e66fc9e4632b8922509d901fa
MIGUELANGEL JOSUÉ CRUZ SAGASTUME <sagastumejosue71@gmail.com>
2026-04-10 13:59:24 -0600

    Se colocaron mas colores y CSS al formulario

--- recepcion.php
+++ recepcion.php
@@ -4,3 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     $name = htmlspecialchars(trim($_POST["nombre"]));
 $lastname = htmlspecialchars(trim($_POST["apellido"]));
 $age = htmlspecialchars(trim($_POST["edad"]));
+
+if (empty($name) || empty($lastname) || $age === 0) {
+    echo "ERROR DEBE LLENAR TODOS LOS DATOS QUE SE LE PIDEN";
+}
