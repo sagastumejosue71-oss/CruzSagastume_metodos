dd344b1c6d00a776e7beafbb55eca51169ff94b3
MIGUELANGEL JOSUÉ CRUZ SAGASTUME <sagastumejosue71@gmail.com>
2026-04-10 14:12:11 -0600

    se procede a utilizar funciones nativas para que cuenta la cantidad de letras y muestre la edad al reves

--- recepcion.php
+++ recepcion.php
@@ -3,8 +3,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
     $name = htmlspecialchars(trim($_POST["nombre"]));
 $lastname = htmlspecialchars(trim($_POST["apellido"]));
-$age = htmlspecialchars(trim($_POST["edad"]));
+$age = intval(trim($_POST["edad"]));
 
 if (empty($name) || empty($lastname) || $age === 0) {
     echo "ERROR DEBE LLENAR TODOS LOS DATOS QUE SE LE PIDEN";
+    exit;
 }
+
+$cantLetnombre = strlen($name);
+$cantLetapellido = strlen($lastname);
+$nameMay = strtoupper($name);
+$lastnameMay = strtoupper($lastname);
+$ageReverse = strval(strrev($age));
+
