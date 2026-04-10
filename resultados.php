e2f7920dedce2656f3af776e4c432e8adeed1e40
MIGUELANGEL JOSUÉ CRUZ SAGASTUME <sagastumejosue71@gmail.com>
2026-04-10 15:36:51 -0600

    erro minuscula

--- resultados.php
+++ resultados.php
@@ -7,13 +7,13 @@ $categoria1= htmlspecialchars(trim($_GET["categoria"]));
 
 $cantLetTermin = strlen ($terminos1);
 $cantLetcat= strlen ($categoria1);
-$Maytermin = strlen($terminos1);
-$maycategoy= strlen($categoria1);
+$Maytermin = strtolower($terminos1);
+$maycategoy= strtolower($categoria1);
 
 echo "<p> Resultados de la Busqueda </p>";
 echo "<p> Busco el nombre de: " .$terminos1 . "  El es un :  .$categoria1 </p>";
 echo "<p> la persona tiene " .$cantLetTermin . " Letras. </p>";
-echo "<p> en minusculas el nombe es :  . $Maytermin </p>";
+echo "<p> en minusculas el nombe es : " . $Maytermin ."</p>";
 
 } else {
     echo "Debe Ingresar un termino. <a href='buscar.html'>Volver</a>";
