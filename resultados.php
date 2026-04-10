d1f40d13f7fc469ce6e6afca29de9925ecc79946
MIGUELANGEL JOSUÉ CRUZ SAGASTUME <sagastumejosue71@gmail.com>
2026-04-10 15:30:24 -0600

    Se finalizo el formulario

--- resultados.php
+++ resultados.php
@@ -1,1 +1,21 @@
-    
+<?php
+
+if (isset ($_GET["terminos"]) && !empty ($_GET["terminos"])){ 
+
+$terminos1 = htmlspecialchars(trim($_GET["terminos"]));
+$categoria1= htmlspecialchars(trim($_GET["categoria"]));
+
+$cantLetTermin = strlen ($terminos1);
+$cantLetcat= strlen ($categoria1);
+$Maytermin = strlen($terminos1);
+$maycategoy= strlen($categoria1);
+
+echo "<p> Resultados de la Busqueda </p>";
+echo "<p> Busco el nombre de: " .$terminos1 . "  El es un:  .$categoria1 </p>";
+echo "<p> la persona tiene " .$cantLetTermin . "Letras. </p>";
+echo "<p> en minusculas el nombe es:  . $Maytermin </p>";
+
+} else {
+    echo "Debe Ingresar un termino. <a href='buscar.html'>Volver</a>";
+}
+?>
